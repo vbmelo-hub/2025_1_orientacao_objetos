8eea48490e44b1e8921b5cd9781a2fd0dea43abe
prof-jpontes <jonas.pontes@ifac.edu.br>
2025-06-06 17:46:54 -0500

    Aula 6_6_2025
    Herança: conceitos, analogia, vantagens, aplicação.
    Início da codificação com herança.

---
+++ unidade-04-heranca-polimorfismo/src-java/figura_geometrica/src/modelo/Circulo.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class Circulo extends FiguraGeometrica {
+
+}
---
+++ unidade-04-heranca-polimorfismo/src-java/figura_geometrica/src/modelo/Quadrilatero.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Quadrilatero extends FiguraGeometrica{
+
+	Quadrilatero(String descricao) {
+		super(descricao);
+	}
+	
+	
+
+}
